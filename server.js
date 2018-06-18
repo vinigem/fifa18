b31248cd7e4f1d416cc503678fb498e9fc3cce63
Vinit Kumar <Kumar_Vinit@cat.com>
2018-06-18 18:18:43 +0530

    mongodb

--- server.js
+++ server.js
@@ -73,8 +73,8 @@ var teamSchema = new Schema({
 var fixtureModel = mongoose.model('fixtures', fixtureSchema, 'fixtures');
 var teamModel = mongoose.model('teams', teamSchema, 'teams');
 
-app.get("/api/getFixtures",function(req,res) {  
-    fixtureModel.find({}, function(err,data) {  
+app.get("/api/getFixtures",function(req, res) {  
+    fixtureModel.find({}.sort({'_id': -1}), function(err, data) {  
         if(err) {  
             res.send(err);  
         } else {                
@@ -84,7 +84,7 @@ app.get("/api/getFixtures",function(req,res) {
 })
 
 app.get("/api/getTeams",function(req,res) {  
-    teamModel.find({}, function(err,data) {  
+    teamModel.find({}.sort({'_id': -1}), function(err, data) {  
         if(err) {  
             res.send(err);  
         } else {                
