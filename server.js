0ab01c92e89673754b17c9197cf7ce753aa10c72
Vinit Kumar <Kumar_Vinit@cat.com>
2018-06-18 17:47:44 +0530

    mongodb

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const database = require('./database.js');
+const mongoose = require("mongoose");
     
 app.use(express.static(__dirname + '/dist/fifa18'));
 app.set('port', process.env.PORT || 5000);
@@ -23,3 +23,72 @@ const forceSSL = function() {
 
 // Instruct the app to use the forceSSL middleware
 app.use(forceSSL());
+
+// Mongo DB
+const db = mongoose.connect("mongodb://admin:passw0rd@ds163480.mlab.com:63480/fifa18", function(err, response){  
+   if(err) {
+        console.log( err);
+    } else {
+        console.log('Connected to ' + db, ' + ', response);
+    }  
+}); 
+
+const Schema = mongoose.Schema;
+
+var fixtureSchema = new Schema({      
+    name: String ,
+    matches: [
+        {
+            num: Number,
+            date: String,
+            time: String,
+            team1: {
+                name: String,
+                code: String
+            },
+            team2: {
+                name: String,
+                code: String
+            },
+            score1: Number,
+            score2: Number,
+            group: String,
+            stadium: {
+                key: String,
+                name: String
+            },
+            city: String,
+            timezone: String
+        }
+    ]  
+}, { versionKey: false });
+
+var teamSchema = new Schema({      
+    id: Number,       
+    name: String,
+    fifaCode: String,
+    flag: String   
+}, { versionKey: false });
+
+var fixtureModel = mongoose.model('fixtures', fixtureSchema, 'fixtures');
+var teamModel = mongoose.model('teams', teamSchema, 'teams');
+
+app.get("/api/getFixtures",function(req,res) {  
+    fixtureModel.find({}, function(err,data) {  
+        if(err) {  
+            res.send(err);  
+        } else {                
+            res.send(data);  
+        }  
+    });  
+})
+
+app.get("/api/getTeams",function(req,res) {  
+    teamModel.find({}, function(err,data) {  
+        if(err) {  
+            res.send(err);  
+        } else {                
+            res.send(data);  
+        }  
+    });  
+})
